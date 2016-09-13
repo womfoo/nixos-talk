1129f5c46defe70b7bcec219b330d59da71386da
Kranium Gikos Mendoza <kranium@gikos.net>
2016-09-14 01:55:33 +0800

    more files

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-pandoc -t revealjd -s exotic.txt -o exotic.html; firefox exotic.html
+# -i incremental
+# -V theme=league
+pandoc -t revealjs --template=templates/default.revealjs -i --no-highlight -s exotic.txt -o exotic.html; firefox exotic.html
